176c9492471eb048888774092dcf2e55fe7057e2
me <remogeissbuhler@gmail.com>
2022-03-17 15:30:43 +0100

    updated test

--- tests/test_jaspar/definitions/modules/mulitpleCmds.py
+++ tests/test_jaspar/definitions/modules/mulitpleCmds.py
@@ -1,13 +1,23 @@
+import warnings
+import jaspar
+
 def test():
     print("=== This is just a test ===")
 
 def repeat(n: int, horizontal=False):
+    # TODO: make automatic int parsing work
+    # TODO: make automatic bool parsing work
+    if isinstance(n, str):
+        warnings.warn("n is string!!")
+        n = int(n)
     text = "Repeat"
     if horizontal:
         text = n * text
     for _ in range(n):
-        print("Repeated!")
+        print(text)
 
 def _nocmdforthat():
     pass
 
+if __name__ == "__main__":
+    jaspar.read_args()
